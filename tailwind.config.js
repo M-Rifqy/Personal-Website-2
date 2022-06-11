0a39a448ee67cb409c2db98d9937ea9e54a6469b
Muhammad Rifqy <2ndrifqy@gmail.com>
2022-06-11 14:03:15 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '50px'
+      },
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
